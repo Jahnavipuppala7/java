ceae5474bd453e0325065f756edbd033cfbc92f6
Jahnavipuppala7 <146073816+Jahnavipuppala7@users.noreply.github.com>
2024-08-08 11:47:21 +0530

    Create 1)Area_of_circle.java

---
+++ 1)Area_of_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int area;
+        System.out.println(3.14*r*r);
+    }
+}
